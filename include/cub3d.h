efd29b4d5f49816d253ef21886dd06a308df1891
KozhInna <inna.kozhina22@gmail.com>
2025-09-04 14:59:15 +0300

    stucts for parsing

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,6 +1,7 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include <stdbool.h>
 # include "libft.h"
 # include "MLX42/MLX42.h"
 
--- include/parsing.h
+++ include/parsing.h
@@ -1,6 +1,34 @@
 #ifndef PARSING_H
 # define PARSING_H
 
-# define PLAYER "./textures/small.png"
+typedef struct s_coords
+{
+	int			x;
+	int			y;
+}				t_coords;
+
+typedef struct s_map
+{
+	char		**map_data;
+	int			width;
+	int			height;
+	t_coords	player;
+	char		direction;
+	int			max_width;
+	bool		is_valid;
+}				t_map;
+
+typedef struct s_data
+{
+	uint32_t	f_color;
+	uint32_t	c_color;
+	char		*no_path;
+	char		*so_path;
+	char		*we_path;
+	char		*ea_path;
+	t_map		map;
+	bool		textures_num[4];
+	bool		colors_num[2];
+}				t_data;
 
 #endif
