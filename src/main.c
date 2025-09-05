aaf8f10d7d2c603f4e6fbbd03df1bde3d1e0d0ab
KozhInna <inna.kozhina22@gmail.com>
2025-09-05 10:35:21 +0300

    initial map with fake data

--- src/main.c
+++ src/main.c
@@ -2,6 +2,18 @@
 
 int	main(void)
 {
-	ft_printf("Running\n");
+	char *map[] = {
+		"1111111111",
+		"1000000001",
+		"1010110001",
+		"1000011101",
+		"100000N001",
+		"1111111111",
+		NULL
+	};
+	t_data	data;
+
+	data.map.map_data = map;
+
 	return (0);
 }
