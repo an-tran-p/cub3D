25fdffc719c3631fae0595839b51341053572fff
KozhInna <inna.kozhina22@gmail.com>
2025-09-10 14:25:24 +0300

    add return value instead of close

--- src/parsing/validate_arg.c
+++ src/parsing/validate_arg.c
@@ -1,49 +1,49 @@
 #include "cub3d.h"
 
-void    check_file_access(char *file_name)
+int	check_file_access(char *file_name)
 {
-    int fd;
+	int	fd;
 
-    fd = open(file_name, O_RDONLY);
-    if (fd == -1)
-    {
-        if (errno == ENOENT)
-            print_error(MSG_NO_FILE);
-        else if (errno == EACCES)
-            print_error(MSG_PERM_DENIED);
-        else if (errno == EISDIR)
-            print_error(MSG_IS_DIR);
-        else
-            print_error(strerror(errno));
-        exit (1);
-    }
-    close(fd);
+	fd = open(file_name, O_RDONLY);
+	if (fd == -1)
+	{
+		if (errno == ENOENT)
+			print_error(MSG_NO_FILE);
+		else if (errno == EACCES)
+			print_error(MSG_PERM_DENIED);
+		else if (errno == EISDIR)
+			print_error(MSG_IS_DIR);
+		else
+			print_error(strerror(errno));
+		exit(1);
+	}
+	return (fd);
 }
 
 void	check_ext(char *file_name)
 {
-	char *ptr;
+	char	*ptr;
 
 	ptr = ft_strrchr(file_name, '.');
 	if (!ptr || (ft_strcmp(ptr, ".cub") != 0))
 	{
-        print_error(MSG_WRONG_EXT);
-		exit (1);
+		print_error(MSG_WRONG_EXT);
+		exit(1);
 	}
 }
 
-void	validate_arg(int argc, char **argv)
+int	validate_arg(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-        print_error(MSG_NO_ARG);
-		exit (1);
+		print_error(MSG_NO_ARG);
+		exit(1);
 	}
 	if (argv[1][0] == '\0')
 	{
-        print_error(MSG_EMPTY_ARG);
-		exit (1);
+		print_error(MSG_EMPTY_ARG);
+		exit(1);
 	}
 	check_ext(argv[1]);
-    check_file_access(argv[1]);
+	return (check_file_access(argv[1]));
 }
