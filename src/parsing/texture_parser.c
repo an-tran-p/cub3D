376a9edb767c38b6468e9e04499cfd710600d9d4
KozhInna <inna.kozhina22@gmail.com>
2025-09-12 15:51:22 +0300

    trum new line in texture path

--- src/parsing/texture_parser.c
+++ src/parsing/texture_parser.c
@@ -1,8 +1,19 @@
 #include "cub3d.h"
 
+int count_path_len(char *line)
+{
+    size_t  len;
+
+    len = 0;
+    while (line[len] && line[len] != '\n')
+        len++;
+    return (len);
+}
+
 int    save_texture(char **path, char *line)
 {
     int     i;
+    size_t     len;
 
     i = 0;
     while (line[i] == ' ')
@@ -12,7 +23,9 @@ int    save_texture(char **path, char *line)
         print_error("Similar texture identifies\n");
         return (1);
     }
-    *path = ft_strdup(line + i);
+    len = count_path_len(line + i);
+    *path = ft_strndup(line + i, len);
+    printf("%s ", *path);
     if (!(*path))
     {
         print_error("Malloc failed\n");
