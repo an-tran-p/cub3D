5f8c4f4d1df9aa4a149ee952e36370bc7fd6bd48
KozhInna <inna.kozhina22@gmail.com>
2025-09-14 12:10:25 +0300

    add check access to texture files and extention, empty texture check

--- include/err_msg.h
+++ include/err_msg.h
@@ -5,12 +5,14 @@
 
 # define MSG_NO_ARG "Include one scene file as argument\n"
 # define MSG_EMPTY_ARG "Specify a scene file with .cub extension\n"
-# define MSG_WRONG_EXT "A scene file should have .cub extension\n"
+# define MSG_WRONG_FILE_EXT "A scene file should have .cub extension\n"
 # define MSG_PERM_DENIED "Permission denied: can't read the file\n"
 # define MSG_NO_FILE "No file found at the provided path\n"
 # define MSG_IS_DIR "The path is a directory. Please provide a scene file\n"
 # define MSG_INVALIB_RGB_R "Invalid RGB: R is outside the range 0-255\n"
 # define MSG_INVALIB_RGB_G "Invalid RGB: G is outside the range 0-255\n"
 # define MSG_INVALIB_RGB_B "Invalid RGB: B is outside the range 0-255\n"
+# define MSG_WRONG_TEXTURE_EXT "A texture file should have .png extension\n"
+# define MSG_NO_ACCESS_TO_TEXTURE "Can't access texture file\n"
 
 #endif
--- src/parsing/texture_parser.c
+++ src/parsing/texture_parser.c
@@ -1,5 +1,32 @@
 #include "cub3d.h"
 
+int	check_texture_access(char *path)
+{
+	int	fd;
+
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+	{
+        print_error(MSG_NO_ACCESS_TO_TEXTURE);
+        return (1);
+	}
+    close(fd);
+	return (0);
+}
+
+int check_texture_ext(char *path)
+{
+	char	*ptr;
+
+	ptr = ft_strrchr(path, '.');
+	if (!ptr || (ft_strcmp(ptr, ".png") != 0))
+	{
+		print_error(MSG_WRONG_TEXTURE_EXT);
+		return (1);
+	}
+    return (0);
+}
+
 int count_path_len(char *line)
 {
     size_t  len;
@@ -18,18 +45,16 @@ int    save_texture(char **path, char *line)
     i = 0;
     while (line[i] == ' ')
         i++;
+    if (line[i] == '\0' || line[i] == '\n')
+        return (print_error("Missing texture path\n"), 1);
     if (*path)
-    {
-        print_error("Similar texture identifies\n");
-        return (1);
-    }
+        return (print_error("Similar texture identifies\n"), 1);
     len = count_path_len(line + i);
     *path = ft_strndup(line + i, len);
     printf("%s ", *path);
     if (!(*path))
-    {
-        print_error("Malloc failed\n");
+        return (print_error("Malloc failed\n"), 1);
+    if (check_texture_ext(*path) != 0 || check_texture_access(*path))
         return (1);
-    }
     return (0);
 }
