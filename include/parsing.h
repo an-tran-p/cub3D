d09e3ee8c02a04c4eb739697f7c932537f5d705d
antran <antranphuong97@gmail.com>
2025-09-28 12:09:32 +0300

    fix the segfault problem when player is too close to wall

--- include/parsing.h
+++ include/parsing.h
@@ -1,12 +1,13 @@
 #ifndef PARSING_H
-#define PARSING_H
+# define PARSING_H
 
-#include <stdio.h>
+# include <stdio.h>
 
 typedef struct s_coords
 {
 	float		x;
 	float		y;
+	float		radius;
 	float		angle;
 	bool		up;
 	bool		down;
@@ -14,41 +15,42 @@ typedef struct s_coords
 	bool		right;
 	bool		l_rotate;
 	bool		r_rotate;
-} t_coords;
+}				t_coords;
 
 typedef struct s_map
 {
-	char **map_data;
-	int width;
-	int height;
-	t_coords player;
-	char direction;
-} t_map;
+	char		**map_data;
+	int			width;
+	int			height;
+	t_coords	player;
+	char		direction;
+}				t_map;
 
 typedef struct s_data
 {
-	uint32_t f_color;
-	uint32_t c_color;
-	char *no_path;
-	char *so_path;
-	char *we_path;
-	char *ea_path;
-	t_list *map_start_node;
-	t_map map;
-} t_data;
+	uint32_t	f_color;
+	uint32_t	c_color;
+	char		*no_path;
+	char		*so_path;
+	char		*we_path;
+	char		*ea_path;
+	t_list		*map_start_node;
+	t_map		map;
+}				t_data;
 
-int validate_arg(int argc, char **argv);
-int print_error(char *err_msg);
-int parse_scene(int fd, t_data *data);
-void free_data(t_data *data);
-int save_color(uint32_t *color, char *line);
-int save_texture(char **texture_path, char *line, int id_pos, t_data *data);
-int is_map(char *line);
-int has_map_chars(char *line);
-int parse_map(t_list *map_start_node, t_data *data);
-int build_map_grid(t_list *map_start_node, t_data *data);
-int validate_walls(t_data *data);
-int validate_map_connectivity(t_data *data);
-void free_map(char **map_copy, int i);
+int				validate_arg(int argc, char **argv);
+int				print_error(char *err_msg);
+int				parse_scene(int fd, t_data *data);
+void			free_data(t_data *data);
+int				save_color(uint32_t *color, char *line);
+int				save_texture(char **texture_path, char *line, int id_pos,
+					t_data *data);
+int				is_map(char *line);
+int				has_map_chars(char *line);
+int				parse_map(t_list *map_start_node, t_data *data);
+int				build_map_grid(t_list *map_start_node, t_data *data);
+int				validate_walls(t_data *data);
+int				validate_map_connectivity(t_data *data);
+void			free_map(char **map_copy, int i);
 
 #endif
--- src/raycasting/game.c
+++ src/raycasting/game.c
@@ -1,90 +1,9 @@
 #include "cub3d.h"
 
-bool	touch(float px, float py, t_map map)
-{
-	int	x;
-	int	y;
-
-	x = px / BLOCK;
-	y = py / BLOCK;
-	if (y < 0 || y >= map.height || x < 0 || x >= map.width)
-		return (true);
-	if (map.map_data[y][x] == '1')
-		return (true);
-	return (false);
-}
-
-float	distance(float x, float y)
-{
-	return (sqrt(x * x + y * y));
-}
-
-float	fixed_dist(float x1, float y1, float x2, float y2, t_game *game)
-{
-	float	delta_x;
-	float	delta_y;
-	float	angle;
-	float	fix_dist;
-
-	delta_x = x2 - x1;
-	delta_y = y2 - y1;
-	angle = atan2(delta_y, delta_x) - game->data->map.player.angle;
-	fix_dist = distance(delta_x, delta_y) * cos(angle);
-	return (fix_dist);
-}
-
-void	view_3d(t_coords *player, t_game *game, float start_x, int i)
-{
-	float	dist;
-	float	height;
-	int		start_y;
-	int		end;
-	float	ray_x;
-	float	ray_y;
-
-	ray_x = player->x;
-	ray_y = player->y;
-	while (!touch(ray_x, ray_y, game->data->map))
-	{
-		ray_x += cos(start_x);
-		ray_y += sin(start_x);
-	}
-	dist = fixed_dist(player->x, player->y, ray_x, ray_y, game);
-	height = (BLOCK / dist) * (WIDTH / 2);
-	start_y = (HEIGHT - height) / 2;
-	end = start_y + height;
-	while (start_y < end)
-	{
-		mlx_put_pixel(game->image, i, start_y, 0x0000FFFF);
-		start_y++;
-	}
-}
-
-void	casting_ray(t_game *game)
-{
-	float		fraction;
-	float		start_x;
-	t_coords	*player;
-	int			i;
-
-	player = &game->data->map.player;
-	fraction = M_PI / 3 / (31 * BLOCK);
-	start_x = player->angle - M_PI / 6;
-	i = 0;
-	while (i < 31 * BLOCK)
-	{
-		if (CUB2D)
-			draw_ray_2d(player, game, start_x);
-		if (!CUB2D)
-			view_3d(player, game, start_x, i);
-		start_x += fraction;
-		i++;
-	}
-}
-
 void	init_game(t_game *game, t_coords *player)
 {
-    mlx_set_setting(MLX_STRETCH_IMAGE, true);
+	player->radius = BLOCK / 3;
+	mlx_set_setting(MLX_STRETCH_IMAGE, true);
 	game->mlx = mlx_init(WIDTH, HEIGHT, "cub3D", true);
 	if (!game->mlx)
 		return ;
@@ -96,7 +15,7 @@ void	init_game(t_game *game, t_coords *player)
 		draw_block(player->x, player->y, 1, 0xFF0000FF, game);
 		draw_map(game, game->data->map.map_data);
 	}
-    load_and_resize_textures(game);
+	load_and_resize_textures(game);
 	mlx_image_to_window(game->mlx, game->image, 0, 0);
 	mlx_key_hook(game->mlx, handle_keyhook, player);
 	mlx_loop_hook(game->mlx, game_loop, game);
